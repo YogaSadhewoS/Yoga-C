affd608de5644fc3828956cda352c2ff558153be
Yoga S <yogasadhewos@gmail.com>
2025-02-25 10:30:58 +0700

    Logic Exercise

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-//Test
-Console.WriteLine("Hello, World!");
+//Day 2
+Console.WriteLine("Masukkan angka: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+int n = 1;
+Console.WriteLine("Hasilnya adalah: ");
+
+while (n <= x)
+{
+    if (n % 3 == 0 && n % 5 == 0) 
+    {
+        Console.Write("foobar");
+    }
+
+    else if (n % 3 == 0)
+    {
+        Console.Write("foo");
+    }
+
+    else if (n % 5 == 0)
+    {
+        Console.Write("bar");
+    }
+
+    else
+    {
+        Console.Write(n);
+    }
+
+    Console.Write(", ");
+    n++;
+}
