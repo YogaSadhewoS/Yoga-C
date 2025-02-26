3c645aa5de87f0a25b52ed7ab18ee8bf1804d7bb
Yoga S <yogasadhewos@gmail.com>
2025-02-26 14:56:18 +0700

    Day 3 Commit

--- ConsoleApp1/Day3/Program.cs
+++ ConsoleApp1/Day3/Program.cs
@@ -1,14 +1,25 @@
-﻿//Day 3
+﻿//Day 3 Constructor
 
 using System;
 
 namespace ConsoleApp1.Day3
 {
-    class Program
+    class Kucing
     {
+        public string Nama;
+        public string Ras;
+        public int Umur;
+
+        public Kucing(string namaKucing, string rasKucing, int umurKucing)
+        {
+            Nama = namaKucing;
+            Ras = rasKucing;
+            Umur = umurKucing;
+        }
         static void Main()
         {
-            Console.WriteLine("Day 3 Gitignore");
+            Kucing kucing1 = new Kucing("Larry", "Hitam", 2);
+            Console.WriteLine($"Namanya {kucing1.Nama}, dia umurnya {kucing1.Umur} tahun, dan dia {kucing1.Ras}");
         }
     }
 }
