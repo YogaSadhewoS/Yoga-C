191bc026045806d62fec51fc70b69a7b6889b29b
Yoga S <yogasadhewos@gmail.com>
2025-02-27 11:31:29 +0700

    Add Multiple Interface

--- ConsoleApp1/Day4/Interface/Program.cs
+++ ConsoleApp1/Day4/Interface/Program.cs
@@ -7,9 +7,25 @@ public interface IBisaBerkendara
     void Berkendara();
 }
 
+public interface IBisaTerbang
+{
+    void Terbang();
+}
+
+public interface IBisaBerenang
+{
+    void Berenang();
+}
+
 public class Motor : IBisaBerkendara
 {
     public void Berkendara() => Console.WriteLine("Motor sedang berjalan");
+}
+
+public class Bebek : IBisaTerbang, IBisaBerenang
+{
+    public void Terbang() => Console.WriteLine("Bebek sedang terbang");
+    public void Berenang() => Console.WriteLine("Bebek sedang berenang");
 }
 
 class Program
@@ -18,5 +34,9 @@ class Program
     {
         Motor myMotor = new Motor();
         myMotor.Berkendara();
+
+        Bebek myBebek = new Bebek();
+        myBebek.Terbang();
+        myBebek.Berenang();
     }
 }
