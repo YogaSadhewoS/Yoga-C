c8247a2ad1e666e2286a21254c9914368e166fc6
YogaSadhewoS <xahacker.alfa@gmail.com>
2025-02-27 13:51:28 +0700

    Enums with User Input

--- ConsoleApp1/Day4/Enum/Program.cs
+++ ConsoleApp1/Day4/Enum/Program.cs
@@ -17,7 +17,25 @@ class Program
 {
     static void Main()
     {
-        Hari hariIni = Hari.Kamis;
-        Console.WriteLine($"Hari ini adalah hari {hariIni}");
+        Console.WriteLine("Masukkan nama hari (Contoh: Minggu atau minggu): ");
+        string? input = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(input))
+        {
+            Console.WriteLine("Input kosong, Mohon diisi dengan benar");
+        }
+
+        else
+        {
+            if (Enum.TryParse(input, true, out Hari hariPilihan))
+            {
+                Console.WriteLine($"Hari yang anda pilih adalah hari {hariPilihan}");
+            }
+            else
+            {
+                Console.WriteLine("Terjadi kesalahan input. Mohon diulangi");
+            }
+        }
+
     }
 }
