1ef8290c708c01061b0381afb5525edea4beafcb
Yoga S <yogasadhewos@gmail.com>
2025-02-28 14:03:49 +0700

    Minor Fix in Func

--- ConsoleApp1/Day5/FuncAction/Program.cs
+++ ConsoleApp1/Day5/FuncAction/Program.cs
@@ -6,11 +6,13 @@ class Program
 {
     static void Main()
     {
-        Func<int, int, int> operasiPenjumlahan = (x, y) => x + y; //Langsung pakai ekspresi Lambda (=>)
-        Console.WriteLine($"Hasil Penjumlahan: {operasiPenjumlahan(6, 4)}");
+        //Func operasiMatematika ditulis sekali
+        Func<int, int, int> operasiMatematika = (x, y) => x + y; //Langsung pakai ekspresi Lambda (=>)
+        Console.WriteLine($"Hasil Penjumlahan: {operasiMatematika(6, 4)}");
 
-        Func<int, int, int> operasiPerkalian = Perkalian; //Passing ke metode perkalian
-        Console.WriteLine($"Hasil Perkalian: {operasiPerkalian(6, 4)}");
+        //Func operasiMatematika di-assign ke metode lain
+        operasiMatematika = Perkalian; //Assign ke metode perkalian
+        Console.WriteLine($"Hasil Perkalian: {operasiMatematika(6, 4)}");
     }
 
     static int Perkalian(int x, int y)
