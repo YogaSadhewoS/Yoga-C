4a119449b5305fea20d1d7c61e6d8082f996e5a7
Yoga S <yogasadhewos@gmail.com>
2025-03-03 14:46:58 +0700

    Add yield return

--- ConsoleApp1/Day6/Enumerator/Program.cs
+++ ConsoleApp1/Day6/Enumerator/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 
 class Program
 {
@@ -14,14 +15,33 @@ class Program
         }*/
 
         //Menggunakan enumerator manual
-        int[] numbers = { 10, 20, 30, 40, 50 };
-        IEnumerator enumerator = numbers.GetEnumerator();
+        /*int[] numbers = { 10, 20, 30, 40, 50 };
+        IEnumerator enumerator = numbers.GetEnumerator(); //GetEnumerator() mengembalikan non-generic IEnumerator
 
         while (enumerator.MoveNext())
         {
-            int num = (int)enumerator.Current;
+            int num = (int)enumerator.Current; //Sehingga harus casting dari object ke int
+            Console.Write(num + ", ");
+        }*/
+
+        //Menggunakan yield return
+        List<int> numbers = new List<int> { 12, 7, 9, 21, 30, 42, 55, 67, 88, 91 };
+
+        foreach (int num in GetOddNumbers(numbers))
+        {
             Console.Write(num + ", ");
         }
 
+    }
+
+    static IEnumerable<int> GetOddNumbers(IEnumerable<int> numbers)
+    {
+        foreach (int num in numbers)
+        {
+            if (num % 2 == 1)
+            {
+                yield return num; //Mengembalikan num satu per satu
+            }
+        }
     }
 }
