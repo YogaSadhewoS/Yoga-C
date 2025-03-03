f4d16caf7130fb7ac24ee6017cdf9e6276866a08
Yoga S <yogasadhewos@gmail.com>
2025-03-03 09:33:32 +0700

    Add more Action Delegate

--- ConsoleApp1/Day5/ActionDelegate/Program.cs
+++ ConsoleApp1/Day5/ActionDelegate/Program.cs
@@ -9,6 +9,11 @@ class Program
         Action<string> cetakPesan = Pesan;
         cetakPesan("Selamat datang di program C#!");
         cetakPesan("Semoga ilmunya bermanfaat");
+
+        Action<int, int> jumlah = (x, y) => Console.WriteLine($"Hasil Penjumlahan: {x + y}");
+        jumlah(5, 3);
+        Action<int, int> kali = (x, y) => Console.WriteLine($"Hasil Perkalian: {x*y}");
+        kali(5, 3);
     }
 
     static void Pesan(string teks)
