b176f342940ca7a892c3f53de2aada1b2d6e7ddd
Yoga S <yogasadhewos@gmail.com>
2025-03-03 14:28:12 +0700

    Add Manual Enumerator

--- ConsoleApp1/Day6/Enumerator/Program.cs
+++ ConsoleApp1/Day6/Enumerator/Program.cs
@@ -1,16 +1,27 @@
 using System;
-using System.Collections.Generic;
+using System.Collections;
 
 class Program
 {
     static void Main()
     {
         //Menggunakan foreach
-        List<string> names = new List<string> { "Alice", "Bob", "Charlie", "David" };
+        /*List<string> names = new List<string> { "Alice", "Bob", "Charlie", "David" };
 
         foreach (string name in names)
         {
             Console.Write(name + ", ");
+        }*/
+
+        //Menggunakan enumerator manual
+        int[] numbers = { 10, 20, 30, 40, 50 };
+        IEnumerator enumerator = numbers.GetEnumerator();
+
+        while (enumerator.MoveNext())
+        {
+            int num = (int)enumerator.Current;
+            Console.Write(num + ", ");
         }
+
     }
 }
