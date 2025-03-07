b669457c577fdaf3a382aabfbbd1a3d17ec623ea
Yoga S <yogasadhewos@gmail.com>
2025-03-07 11:44:24 +0700

    Add More List

--- ConsoleApp1/Day10/List/Program.cs
+++ ConsoleApp1/Day10/List/Program.cs
@@ -1,6 +1,7 @@
 using System;
 
-List<string> names = new List<string> {"Yoga", "Ogay", "Gayo"}; //Case sensitive
+//List<string> Case
+/*List<string> names = new List<string> {"Yoga", "Ogay", "Gayo"}; //Case sensitive
 names.Add("Ayog");
 names.Remove("Ogay");
 
@@ -20,4 +21,23 @@ if (names.Contains(searchName)) //Cek data di list
 else
 {
     Console.WriteLine($"Nama {searchName} tidak ditemukan");
+}*/
+
+//List<int> Case
+List<int> numbers = new List<int>();
+
+for (int i = 0; i < 6; i++) //Biar user bisa masukin banyak input
+{
+    Console.Write($"Masukkan angka ke-{i + 1}: ");
+    if (int.TryParse(Console.ReadLine(), out int input))
+    {
+        numbers.Add(input);
+    }
+    else
+    {
+        Console.WriteLine("Input tidak valid, mohon masukkan angka");
+        i--; //Mengulangi input karena tidak valid
+    }
 }
+double rataRata = numbers.Average();
+Console.WriteLine($"\nRata-rata dari bilangan yang dimasukkan: {rataRata:F2}"); //2 angka belakang koma
