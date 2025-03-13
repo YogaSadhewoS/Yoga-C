eea77e58f5390ef934da69bdb97d927f80c9c488
YogaSadhewoS <xahacker.alfa@gmail.com>
2025-03-13 14:15:31 +0700

    Add Parallel Task Case

--- ConsoleApp1/Day16/Async/Program.cs
+++ ConsoleApp1/Day16/Async/Program.cs
@@ -3,7 +3,8 @@ using System.Threading.Tasks;
 
 class Program
 {
-    static async Task Main()
+    //Basic Async Case
+    /*static async Task Main()
     {
         Console.WriteLine("Masukkan angka sebagai basis: ");
         int basis = int.Parse(Console.ReadLine());
@@ -22,12 +23,46 @@ class Program
     {
         await Task.Delay(2000);
 
-        int hasil = 1;
+        int hasil = 1; //Agar hasil 1 ketika input 0
         for (int i = 0; i < b; i++ )
         {
             hasil *= a;
         }
 
         return hasil;
+    }*/
+
+    //Parallel task
+    static async Task Main()
+    {
+        Console.WriteLine("Menyiapkan aplikasi. Mohon tunggu sebentar!");
+
+        Task konfigurasiAplikasi = ConfigureApplication();
+        Task konekKeDatabase = ConnectToDatabase();
+        Task ngaturCache = CacheInitialization();
+
+        await Task.WhenAll(konfigurasiAplikasi, konekKeDatabase, ngaturCache);
+        Console.WriteLine("Aplikasi siap digunakan!");
+    }
+
+    static async Task ConfigureApplication()
+    {
+        Console.WriteLine("Memuat konfigurasi aplikasi...");
+        await Task.Delay(1500);
+        Console.WriteLine("Konfigurasi selesai");
+    }
+
+    static async Task ConnectToDatabase()
+    {
+        Console.WriteLine("Menghubungkan ke database...");
+        await Task.Delay(2500);
+        Console.WriteLine("Database terhubung");
+    }
+
+    static async Task CacheInitialization()
+    {
+        Console.WriteLine("Menginisialisasi cache...");
+        await Task.Delay(1000);
+        Console.WriteLine("Cache sudah diinisialisasi");
     }
 }
