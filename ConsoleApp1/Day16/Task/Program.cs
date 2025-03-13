b24e82110871c3b1cace620bd404ace503cf5a3c
YogaSadhewoS <xahacker.alfa@gmail.com>
2025-03-13 11:11:04 +0700

    Add Task with Generic

--- ConsoleApp1/Day16/Task/Program.cs
+++ ConsoleApp1/Day16/Task/Program.cs
@@ -6,7 +6,8 @@ class Program
 {
     static void Main()
     {
-        Task task1 = Task.Run(() =>
+        //Basic task w/o async
+        /*Task task1 = Task.Run(() =>
         {
             Console.WriteLine("Tugas 1 dimulai");
             Thread.Sleep(1000); //Pakai thread sleep karena Main() ga pake async
@@ -22,6 +23,33 @@ class Program
 
         task1.Wait();
         task2.Wait();
+        Console.WriteLine("Program selesai");*/
+
+        //Generic Task<int> so the output will be int
+        Console.WriteLine("Masukkan angka bulat positif: ");
+        int angka;
+
+        while(!int.TryParse(Console.ReadLine(), out angka) || angka < 0)
+        {
+            Console.WriteLine("Input yang bener blok");
+        }
+
+        Task<int> task = Task.Run(() =>
+        {
+            Console.WriteLine($"Menghitung faktorial dari angka {angka}");
+            Thread.Sleep(1000);
+
+            int result = 1;
+            for (int i = angka; i > 0; i--)
+            {
+                result *= i;
+            }
+            return result;
+
+        });
+
+        int hasil = task.Result;
+        Console.WriteLine($"Output: {hasil}");
         Console.WriteLine("Program selesai");
     }
 }
