cb9b98fbb8ff827feaf1bec5c6544d9111665ec9
YogaSadhewoS <xahacker.alfa@gmail.com>
2025-03-19 09:31:38 +0700

    Fix minor problem

--- ConsoleApp1/IndividualProject/IBoard.cs
+++ ConsoleApp1/IndividualProject/IBoard.cs
@@ -5,6 +5,8 @@ namespace BattleshipGame
         bool PlaceShip(IShip ship, int row, int column, Orientation orientation);
         CellStatus GetCellStatus(int row, int column);
         void SetCellStatus(int row, int column, CellStatus status);
+
+        // Mengembalikan kapal yang ditempatkan pada posisi (row, column), atau null jika tidak ada
         IShip? GetShipAt(int row, int column);
         bool IsPositionValid(int row, int column);
         IReadOnlyList<IShip> GetAllShips();
